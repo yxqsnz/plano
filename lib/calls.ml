1d3e493e10d193f5751a06793fe66d1023c23da7
yxqsnz <yxqsnz@gmail.com>
2023-02-14 21:15:11 -0300

    fix: Not_found when compiler are not in path

--- lib/calls.ml
+++ lib/calls.ml
@@ -1,14 +1,17 @@
-exception UnameToFindLibrary of string
+exception UnableToFindLibrary of string
+exception UnableToFindInPath of string
 
 let find_path it =
-    Sys.getenv("PATH")
+    try Sys.getenv("PATH")
     |> String.split_on_char ':' 
     |> List.map (fun x -> Array.to_list (Sys.readdir x))
     |> List.flatten
     |> List.find (fun x -> Filename.check_suffix x it)
+    with Not_found -> raise (UnableToFindInPath ("can't find " ^ it ^ " in $PATH."))
+
 
 let pkgconf pkg = 
     let output = Os.capture_output_of ("pkgconf --cflags --libs " ^ pkg)
     in match output with
-     | "" ->  raise (UnameToFindLibrary ("uname to find library " ^ pkg))
+     | "" ->  raise (UnableToFindLibrary ("uname to find library " ^ pkg))
      | out -> out
